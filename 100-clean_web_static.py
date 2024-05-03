bba385a04cc6d220858964028c51fbf241bb5144
simowardi <daoist32@gmail@gmail.com>
2024-05-03 12:55:14 -0700

    task 100 Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Deletes out-of-date archives
+Fabfile to delete out-of-date archives.
 fab -f 100-clean_web_static.py do_clean:number=2
     -i ssh-key -u ubuntu > /dev/null 2>&1
 """
@@ -15,6 +15,7 @@ def do_clean(number=0):
     """Delete out-of-date archives.
     Args:
         number (int): The number of archives to keep.
+
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
