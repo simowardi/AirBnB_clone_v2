d55c37d29e942487c4ec899f2f9894fc70118575
simowardi <daoist32@gmail@gmail.com>
2024-05-03 12:10:51 -0700

    updt 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -26,7 +26,8 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception as e:
+        print(f"Error occurred: {e}")
         return None
 
 
@@ -54,7 +55,8 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print(f"Error occurred: {e}")
         return False
 
 
