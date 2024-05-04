6f6435670dfaeb3aacc89f980ef35906093607e8
simowardi <daoist32@gmail@gmail.com>
2024-05-04 14:02:16 -0700

    updt 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-# Fabfile to create and distribute an archive to a web server.
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+"""
 from datetime import datetime
 from fabric.api import env, local, put, run
 
@@ -25,37 +30,30 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        bool: True if successful, False otherwise.
-    """
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
     try:
-        if not os.path.isfile(archive_path):
-            return False
-        file = archive_path.split("/")[-1]
-        name = file.split(".")[0]
-
-        put(archive_path, "/tmp/{}".format(file))
-        run("rm -rf /data/web_static/releases/{}/".format(name))
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
-        run("rm /tmp/{}".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception as e:
-        print(f"Failed to deploy: {e}")
+        print(f"Failed to create archive: {e}")
         return False
 
 
 def deploy():
-    """Creates and distributes an archive to the web servers"""
-    file = do_pack()
-    if file is None:
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-    return do_deploy(file)
+    return do_deploy(archive_path)
