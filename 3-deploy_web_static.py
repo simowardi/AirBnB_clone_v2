ecf2c19607756c98a25ff32d8756a85c1271222c
simowardi <daoist32@gmail@gmail.com>
2024-05-04 18:14:40 -0700

    updt task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -18,20 +18,19 @@ def do_pack():
         str: The filename of the created archive
              if successful, None otherwise.
     """
-    local('sudo mkdir -p version')
-
-    time = datetime.now()
-    tm_str = time.strftimeme('%Y%m%d%H%M%S')
-
-    locla(f'sudo tar -cvzf version/webstatic_{tm_str}.tgz web_static')
-
-    file_path = f"version/web_static_{tm_str}.trz"
-
-    file_size = os.path.getsize(f_path)
-
-    print(f"web_static packed: {file_path} -> {file_size}")
-
-    return file_path
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
 
 
 def do_deploy(archive_path):
