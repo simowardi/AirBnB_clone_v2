f610a52a41820fb262e6ce272e3f2e880682981f
simowardi <daoist32@gmail@gmail.com>
2024-05-19 13:56:51 -0700

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,23 +4,17 @@
     Routes:
     /: Displays 'Hello HBNB!'
 """
-
 from flask import Flask
+
 
 # Create an instance of the Flask class for the web application.
 app = Flask(__name__)
 '''The Flask application instance.'''
-
-# Disable strict slashes, so routes will work with or without a trailing slash.
 app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
-    '''The home page route.
-
-    Returns:
-        str: The greeting message "Hello HBNB!".
-    '''
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
