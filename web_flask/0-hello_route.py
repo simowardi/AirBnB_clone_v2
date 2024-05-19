78a6966c5b4f7787c4ff5724013244d882616cf0
simowardi <daoist32@gmail@gmail.com>
2024-05-19 14:00:03 -0700

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,15 +7,15 @@
 from flask import Flask
 
 
-# Create an instance of the Flask class for the web application.
 app = Flask(__name__)
 '''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def index():
-    """returns Hello HBNB!"""
+    '''The home page.'''
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
