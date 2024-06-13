5241e2536e899e4fb80527591a8785ddb47387e6
simowardi <132934190+simowardi@users.noreply.github.com>
2024-06-13 21:40:30 +0100

    Update 0-hello_route.py
    updt

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    """Displays Hello HBNB!'"""
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
